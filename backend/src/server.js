2cf5c1f53b501f42e91539e2ffef2c2910f86320
eduardo <eduwric@gmail.com>
2019-10-02 16:17:33 -0300

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path'); 
 
 const routes = require('./routes');
 
@@ -10,7 +12,9 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0-jwyto.mongodb.net/test?retr
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333, () => {
